0a0fbda280aa6f416a12202ac81257faf5abb7e9
Sahil Agarwal <sahil.agarwal@thesmartcube.com>
2018-04-04 11:17:47 +0530

    create functions for better readability

--- flow_control/for/for.go
+++ flow_control/for/for.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func forBasicExample() {
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
@@ -11,15 +11,42 @@ func main() {
 	// i < 10 is the condition part of the for loop
 	// i++ is the post part of the for loop
 	fmt.Println(sum)
+}
 
-	sum2 := 1
-	for sum2 < 1000 {
-		sum2 += sum2
+func forWithoutPostPartTry() {
+	sum := 0
+	for i := 0; i < 10; {
+		sum += i
+		i = i + 1
 	}
-	fmt.Println(sum2)
+	// i:=0 is the init part of the for loop
+	// i < 10 is the condition part of the for loop
+	// i++ is the post part of the for loop
+	fmt.Println(sum)
+}
 
+func forWhileExample() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+func forInfiniteLoopExample() {
 	for {
 		fmt.Println("This is an infinite loop, but breaks due to break stmt!")
 		break
 	}
+}
+
+func main() {
+	fmt.Print("forBasicExample: ")
+	forBasicExample()
+	fmt.Print("forWhileExample: ")
+	forWhileExample()
+	fmt.Print("forInfiniteLoopExample: ")
+	forInfiniteLoopExample()
+	fmt.Print("forWithoutPostPartTry: ")
+	forWithoutPostPartTry()
 }
