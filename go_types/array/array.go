a6538b9a22646b556f081d2950c6478cc9eb34f0
Sahil Agarwal <sahil.agarwal@thesmartcube.com>
2018-06-21 11:58:40 +0530

    arrays - comments and added exampleDefaultValues

--- go_types/array/array.go
+++ go_types/array/array.go
@@ -4,11 +4,16 @@ import "fmt"
 
 func main() {
 	var a [2]string
+	// An array's length is part of its type, so arrays cannot be resized.
 	a[0] = "Hello"
 	a[1] = "World"
+	// a[2] = "Gives error when uncommented"
 	fmt.Println(a[0], a[1])
 	fmt.Println(a)
 
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+
+	exampleDefaultValues := [6]int{2, 3, 5, 7}
+	fmt.Println(exampleDefaultValues)
 }
